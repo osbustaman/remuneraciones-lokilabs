5f0c785bc99e5a8b08e2ccc96298cf1c655f4064
dmentron <osvaldo.bustamante.n@gmail.com>
2023-10-17 23:45:47 -0300

    se suben los fomrs de mis datos

--- applications/usuario/admin.py
+++ applications/usuario/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from applications.usuario.models import Colaborador, UsuarioEmpresa, FamilyResponsibilities, ConceptUser
+from applications.usuario.models import Colaborador, UsuarioEmpresa, FamilyResponsibilities, ConceptUser, Contact
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(Colaborador)
 admin.site.register(UsuarioEmpresa)
 admin.site.register(FamilyResponsibilities)
 admin.site.register(ConceptUser)
+admin.site.register(Contact)
--- applications/usuario/api/api.py
+++ applications/usuario/api/api.py
@@ -1,11 +1,12 @@
 
-from django.db.models import F, Value, CharField
+from django.db.models import F, Value, CharField, Q
 from django.db.models.functions import Concat
 
 from rest_framework.response import Response
 from rest_framework import generics, status
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
+from applications.base.models import Comuna, Region
 
 from applications.empresa.models import Afp
 from applications.usuario.api.serializer import AfpSerializer
@@ -34,35 +35,58 @@ class AfpDetailApiView(generics.RetrieveAPIView):
 @permission_classes([AllowAny])
 class ApiGetDataUserPage(generics.ListAPIView):
     queryset = User.objects.all()
-    serializer_class = AfpSerializer
 
     def get(self, request, *args, **kwargs):
         try:
             object = self.queryset.filter(id=self.kwargs['pk'])
             if object.exists():
                 object = object.first()
-
                 last_name = (object.last_name).split(" ")
 
                 object_colaborator = Colaborador.objects.get(user = object)
+                object_contact = Contact.objects.filter(user = object, con_contact_type = 2).first()
 
+                object_contact_phone = Contact.objects.filter(
+                    Q(user=object) & (Q(con_contact_type=3) | Q(con_contact_type=4))
+                ).first()
 
-                object_contact = Contact.objects.filter(user = object)
+                object_regiones = Region.objects.all()
+                regiones_dict_list = list(object_regiones.values())
+
+                object_comunas = Comuna.objects.filter(region = object_colaborator.region)
+                object_comunas_dict_list = list(object_comunas.values())
 
                 data = {
                     "username": object.username,
                     "first_name": object.first_name,
                     "first_last_name": last_name[0] if len(last_name) > 0 else '',
                     "second_last_name": last_name[1] if len(last_name) > 1 else '',
-
                     "col_fechanacimiento": object_colaborator.col_fechanacimiento,
                     "col_sexo": object_colaborator.get_col_sexo_display().title(),
-                    "col_estadocivil": object_colaborator.get_col_estadocivil_display(),
+                    "col_estadocivil": str(object_colaborator.col_estadocivil),
+                    "phone_user": object_contact_phone.con_phone_contact if object_contact_phone.con_phone_contact else '',
+                    "email_corporativo": object.email,
+                    "email_personal": object_contact.con_mail_contact if object_contact.con_mail_contact else '@',
+                    "col_direccion": object_colaborator.col_direccion,
+                    "col_region": object_colaborator.region.re_id,
+                    "col_comuna": object_colaborator.comuna.com_id,
+                    "col_banco": object_colaborator.banco.ban_nombre,
+                    "col_formapago": object_colaborator.col_formapago,
+                    "col_formapago_name": object_colaborator.get_col_formapago_display(),
+                    "col_tipocuenta_name": object_colaborator.get_col_tipocuenta_display(),
+                    "col_cuentabancaria": object_colaborator.col_cuentabancaria,
+                    "regiones_list_dict": regiones_dict_list,
+                    "comunas_dict_list": object_comunas_dict_list,
 
 
+                    
                 }
-                return Response(data, status=status.HTTP_201_CREATED)
+                return Response(data, status=status.HTTP_200_OK)
             else:
-                raise IndexError("La AFP no existe")
+                raise IndexError("El usuario no existe")
+        except Colaborador.DoesNotExist as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Contact.DoesNotExist as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
         except IndexError as e:
             return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
