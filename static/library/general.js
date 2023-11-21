6d45e47176662d197eacb62fe1a0efbde07ca7b0
osbustaman4 <osvaldo.bustamante.n@gmail.com>
2023-11-21 17:01:41 -0300

    upload

--- static/library/general.js
+++ static/library/general.js
@@ -1,4 +1,13 @@
 const get_dict_form = (form_name) => {
+    /**
+     * 
+     * :form_name: id del formulario
+     * 
+     * 
+     * :return
+     *      diccionario con los inputs del formulario
+     * 
+    */
     const formulario = document.getElementById(form_name);
     const elementos = formulario.elements;
     const diccionario = {};
@@ -14,4 +23,20 @@ const get_dict_form = (form_name) => {
         }
     }
     return diccionario;
+}
+
+
+const save_data = (form_name) => {
+    const dict_form = get_dict_form(form_name);
+    const _validate_inputs = validate_inputs(form_name);
+
+    console.log(`${_validate_inputs}`);
+
+    if(!_validate_inputs){
+        console.log(`no se envian datos`);
+
+
+    }else{
+        console.log(`se envia datos`);
+    }
 }
